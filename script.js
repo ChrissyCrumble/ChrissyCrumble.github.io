e255497d530ced7cf32eea3235e00bc3bd03855a
boblox <84726280+bobloxmonke@users.noreply.github.com>
2022-07-02 21:22:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,4 @@ function onload_event()
 {
     document.getElementById("particles-js").style.opacity = "1";
     document.getElementById("main").style.opacity = "1";
-    
-    setTimeout(() => {
-        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
-    }, 3000);
 }
